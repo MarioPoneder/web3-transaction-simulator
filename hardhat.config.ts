f984876a004c5ef614e191218a4d79125fff5084
Mario Poneder <m.poneder@decentra.vision>
2022-11-20 23:01:26 +0100

    chore: added network forking configuration to hardhat.config.ts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,32 @@
 import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import { config as dotenvConfig } from "dotenv";
+import { resolve } from "path";
+
+
+dotenvConfig({ path: resolve(__dirname, "./.env") });
+
+// Ensure that we have all the environment variables we need.
+const networkChainId: number | undefined = process.env.NETWORK_CHAIN_ID;
+if (!networkChainId) {
+  throw new Error("Please set your NETWORK_CHAIN_ID in a .env file");
+}
+
+const networkJsonRpcUrl: string | undefined = process.env.NETWORK_JSON_RPC_URL;
+if (!networkJsonRpcUrl) {
+  throw new Error("Please set your NETWORK_JSON_RPC_URL in a .env file");
+}
+
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.17",
+  networks: {
+    hardhat: {
+      chainId: +networkChainId,
+      forking: {
+        url: networkJsonRpcUrl,
+      },
+    },
+  },
 };
+
 
 export default config;
