eb1551d13f9ea16dd8a4c8899505183dda2efe2b
Mario Poneder <m.poneder@decentra.vision>
2022-12-27 22:14:21 +0100

    feat: added URL parsing to 'connect-metamask' script

--- connect-metamask/main.js
+++ connect-metamask/main.js
@@ -33,8 +33,10 @@ const isMobile = () => {
   return check;
 };
 
+const urlParams = new URLSearchParams(window.location.search);
+
 const chainId = '0x1';
-const rpcURL = 'http://localhost:8545';
+const rpcURL = urlParams.get('rpcUrl') || 'http://localhost:8545';
 const networkName = 'Transaction Simulator';
 const currencyName = 'ETH';
 const currencySymbol = 'ETH';
